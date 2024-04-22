a7607e211a039c689896ff06b21928775b1d35c7
Aiz <dev@aiz.moe>
2024-04-22 13:20:32 +0200

    fix: make it work
    wasn's working because i used wrong language server or something and it messed the code up.

--- jokers.lua
+++ jokers.lua
@@ -1,34 +1,35 @@
 --- STEAMODDED HEADER
 --- MOD_NAME: Aiz Jokers
---- MOD_ID: AIZ_JOKERS
+--- MOD_ID: JAIZ
 --- MOD_AUTHOR: [Aiz]
 --- MOD_DESCRIPTION: jank jokers
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
-function SMODS.INIT.AIZ_JOKERS()
+
+function SMODS.INIT.JAIZ()
 	local loc_def = {
 		["name"] = "Joker Test",
 		["text"] = {
 			[1] = "AHAHA",
 			[2] = "Jokes on",
-			[3] = "{C:attention}You{}!",
-		},
+			[3] = "{C:attention}You{}!"
+		}
 	}
 
 	-- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
 	local joker_test = SMODS.Joker:new("Joker Test", "test", {}, {
 		x = 0,
-		y = 0,
+		y = 0
 	}, loc_def, 1, 4)
 
-	SMODS.Sprite:new("j_test", SMODS.findModByID("AIZ_JOKERS").path, "j_aha.png", 71, 95, "asset_atli"):register()
+	SMODS.Sprite:new("j_test", SMODS.findModByID("JAIZ").path, "j_aha.png", 71, 95, "asset_atli"):register();
 
 	joker_test:register()
 
-	SMODS.Jokers.j_test.effect = function(self, context)
-		sendDebugMessage("Hello !")
+	SMODS.Jokers.j_test.set_ability = function(self, context)
+		sendDebugMessage("Hello !", 'MyLogger')
 	end
 
 	SMODS.Jokers.j_test.calculate = function(self, context)
@@ -37,7 +38,7 @@ function SMODS.INIT.AIZ_JOKERS()
 				mult_mod = 20,
 				card = self,
 				colour = G.C.RED,
-				message = "AHAHAHAH",
+				message = "AHAHAHAH"
 			}
 		end
 	end
