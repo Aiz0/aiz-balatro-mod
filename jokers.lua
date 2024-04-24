01e854492b23cf31b35ad404a27a4563856b0918
Aiz <dev@aiz.moe>
2024-04-24 14:34:15 +0200

    refactor(jokers): use helper for blj
    kinda not working atm.

--- jokers.lua
+++ jokers.lua
@@ -312,6 +312,59 @@ function Jokers()
 			return {}
 		end
 	end
+	if config.backwardsLongJump then
+		-- Backwards Long Jump (BLJ)
+		-- Gain mult on trigger reset after scored
+		-- TODO implement the code for this. seems kinda hard
+
+		-- Create Joker
+		local blj = {
+			loc = {
+				name = "BLJ",
+				text = {
+					"This Joker gains {X:mult,C:white}X#2#{} Mult if",
+					"a card is triggered during scoring",
+					"{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)",
+				}
+			},
+			ability_name = "AIZ Backwards Long Jump",
+			slug = "aiz_blj",
+			ability = {
+				extra = {
+					Xmult = 1,
+					Xmult_mod = 0.1,
+				}
+			},
+			rarity = 4,
+			cost = 15,
+			unlocked = true,
+			discovered = true,
+			blueprint_compat = false,
+			eternal_compat = true,
+		}
+		-- Initialize Joker
+		init_joker(blj, true)
+
+		-- Set local variables
+		SMODS.Jokers.j_aiz_blj.loc_def = function(card)
+			return { card.ability.extra.Xmult_mod, card.ability.extra.Xmult }
+		end
+
+		-- Calculate
+		SMODS.Jokers.j_aiz_blj.calculate = function(card, context)
+			if SMODS.end_calculate_context(context) and card.ability.extra > 1 then
+				return {
+					message = localize {
+						type = 'variable',
+						key = 'a_xmult',
+						vars = { card.ability.extra.Xmult }
+					},
+					Xmult_mod = card.ability.extra.Xmult,
+				}
+			end
+		end
+	end
+
 	-- if config. then
 	-- 	--
 	-- 	--
@@ -365,51 +418,6 @@ end
 function SMODS.INIT.JAIZ()
 	if config.allEnabled then
 		if config.jokersEnabled then Jokers() end
-	end
-
-
-	local enabled = true
-	if enabled then
-		-- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
-		local joker_test = SMODS.Joker:new("Joker Test", "test", { extra = { Xmult = 1, Xmult_mod = 0.1 } }, {
-			x = 0,
-			y = 0
-		}, {
-			name = "BLJ",
-			text = {
-				"This Joker gains {X:mult,C:white}X#2#{} Mult if",
-				"a card is triggered during scoring",
-				"{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)",
-			}
-		}, 1, 4)
-
-		SMODS.Sprite:new("j_test", SMODS.findModByID("JAIZ").path, "j_aha.png", 71, 95, "asset_atli"):register();
-
-		joker_test:register()
-
-		SMODS.Jokers.j_test.loc_def = function(card)
-			return { card.ability.extra.Xmult, card.ability.extra.Xmult_mod }
-		end
-
-		SMODS.Jokers.j_test.calculate = function(card, context)
-			if context.individual then
-				if context.cardarea == G.play or context.cardarea == G.hand then
-					card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
-					return {
-						extra = { focus = card, message = "Yahoooo!!!" },
-						card = card,
-						colour = G.C.RED
-					}
-				end
-			elseif SMODS.end_calculate_context(context) and card.ability.extra.Xmult > 1 then
-				return {
-					Xmult_mod = card.ability.extra.Xmult,
-					card = card,
-					colour = G.C.mult,
-					message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult } }
-				}
-			end
-		end
 	end
 end
 
