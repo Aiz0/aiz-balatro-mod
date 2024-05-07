e473d4af147e27e5ca7e99d63b347cd4fd1c135b
Aiz <dev@aiz.moe>
2024-05-07 15:52:48 +0200

    feat(site): starting point for joker display

---
+++ site/src/jokers.ts
@@ -0,0 +1,11 @@
+const jokers = [
+  {
+    name: "Chill Joker",
+    text: [
+      "{X:mult,C:white}X#1#{} divided by game speed",
+      "{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)",
+    ],
+    rarity: 1,
+  },
+];
+export { jokers };
