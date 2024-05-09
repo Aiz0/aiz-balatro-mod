02856fe06c979ae75d461116688f86ceafedcec6
Aiz <dev@aiz.moe>
2024-05-09 23:35:55 +0200

    feat(site): update site domain

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://aiz0.github.io",
-  base: "aiz-balatro-mod",
+  site: "https://balatro.aiz.moe",
 });
