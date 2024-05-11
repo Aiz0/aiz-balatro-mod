e296d35a8a79c0e9a53fe435924e420f4e991acc
Aiz <dev@aiz.moe>
2024-05-12 00:05:09 +0200

    fix(chill joker): missing word

--- site/src/jokers.ts
+++ site/src/jokers.ts
@@ -2,8 +2,8 @@ const jokers = [
   {
     name: "Chill Joker",
     text: [
-      "{X:mult,C:white}X2{} divided by",
-      "game speed",
+      "{X:mult,C:white}X2{} Mult divided",
+      "by game speed",
       "{C:inactive}(Currently {X:mult,C:white}X2{C:inactive} Mult)",
     ],
     badge: "Rare",
