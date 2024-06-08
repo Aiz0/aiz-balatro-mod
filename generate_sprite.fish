7e0b3adfecc9287141b0eb5c60f2eeecccd86941
Aiz <dev@aiz.moe>
2024-06-08 21:48:15 +0200

    chore: generate atlas instead of individual files

--- generate_sprite.fish
+++ generate_sprite.fish
@@ -1,6 +1,3 @@
 #!/bin/fish
-for file in assets/*.aseprite
-    set slug (basename $file .aseprite)
-    aseprite -b $file --save-as assets/1x/$slug.png
-    aseprite -b $file --scale 2 --save-as assets/2x/$slug.png
-end
+aseprite --batch ./assets/j_*.aseprite --sheet assets/1x/jokers.png
+aseprite --batch ./assets/j_*.aseprite --scale 2 --sheet assets/2x/jokers.png
