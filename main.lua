bc1f03fda9dd168ef84caaa2f07a091a04b59baa
Aiz <dev@aiz.moe>
2024-08-06 23:42:49 +0200

    chore: update version

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@
 --- MOD_AUTHOR: [Aiz]
 --- MOD_DESCRIPTION: A mod adding Jokers I thought would be funny, cool or just felt like making. Has not been balance tested.
 --- BADGE_COLOUR: 0ea5e9
---- VERSION: 0.7.0
+--- VERSION: 0.8.0
 --- LOADER_VERSION_GEQ: 1.0.0
 
 ----------------------------------------------
