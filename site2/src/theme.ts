01b181f412e497e19e9a28e6f1b8be1e649c821a
Aiz <dev@aiz.moe>
2026-02-09 15:58:43 +0100

    feat(site): pass color to pixelBorderWrapper and use as shadow

---
+++ site2/src/theme.ts
@@ -0,0 +1,61 @@
+export const colors = {
+  balatro: {
+    red: "",
+    mult: "#fe5f55",
+    blue: "",
+    chips: "#009dff",
+    green: "#4bc292",
+    money: "#f3b958",
+    gold: "#eac058",
+    attention: "#ff9a00",
+    purple: "",
+    white: "#ffffff",
+    inactive: "#88888899",
+
+    //suits
+    spades: "#403995",
+    hearts: "#f03464",
+    clubs: "#235955",
+    diamonds: "#f06b3f",
+
+    // cards
+    tarot: "#a782d1",
+    planet: "#13afce",
+    spectral: "#5d5dff",
+
+    // rarity
+    common: "#009dff",
+    uncommon: "#4bc292",
+    rare: "#fe5f55",
+    legendary: "#b26cbb",
+
+    "dark-red": "#ca0d01",
+
+    "dark-blue": "#005e99",
+
+    yellow: "#ffff00",
+    orange: {
+      other: "#fda200",
+      voucher: "#fd682b", // voucher
+    },
+
+    black: {
+      DEFAULT: "#374244",
+      light: "#4f6367", // l_black
+    },
+    grey: {
+      DEFAULT: "#d8d8d8", // outline
+      dark: "#828282", // outline
+      other: "#5f7377",
+      joker: "#bfc7d5", // joker_gray
+      enhancement: "#8389dd",
+    },
+    brown: "#cb724c", // voucher thingy
+
+    // Gonna Keep these as is
+
+    eternal: "#c75985",
+    deck: "#9bb6bd", // pack aswell
+    joker: "#708b91",
+  },
+};
